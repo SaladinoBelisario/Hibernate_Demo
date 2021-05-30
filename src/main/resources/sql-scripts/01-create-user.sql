3460e8df789d82a70b4b9a36a1396275d72c7a5d
Saladino Belisario <SaladinoBelisario@protonmail.ch>
2021-05-29 21:24:51 -0500

    Second Commit

---
+++ src/main/resources/sql-scripts/01-create-user.sql
@@ -0,0 +1,3 @@
+CREATE USER 'hbstudent'@'localhost' IDENTIFIED VIA mysql_native_password USING '*049A339979DB8FD036487A71F33E880BE9688FCC';
+
+GRANT ALL PRIVILEGES ON * . * TO 'hbstudent'@'localhost';
